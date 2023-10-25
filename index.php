92d25ba5f50e4647b2b5977c92ba51d0e91ad323
Miguel Ángel Guaita <miguelangelguaivi@gmail.com>
2023-10-25 12:24:35 +0200

    Añadiendo index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicio modelos</title>
+</head>
+<body>
+    <h1>Pagina inicio modelos</h1>
+    <p>Entrar con ejemplo - ejemplo (user - password)</p>
+    <br><br>
+    <form action="enlaces.php" method="post">
+        <label for="username">Nombre de Usuario: </label>
+        <input type="text" name="username">
+        <br>
+        <label for="password">Contraseña</label>
+        <input type="text" name="password">
+        <br>
+        <input type="submit" value="Send">
+    </form>
+
+</body>
+</html>
