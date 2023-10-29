8025379ed73022098b88edff366343a3f2b2614a
pabcrudel <pabcrudel@gmail.com>
2023-10-29 20:19:40 +0100

    Quitar la llamada a la creacion de la BD

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
-<?php
-    include 'creacionDatos.php';
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicio modelos</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Página inicio modelos</title>
 </head>
 <body>
-    <h1>Pagina inicio modelos</h1>
-    <p>Entrar con ejemplo - ejemplo (user - password)</p>
-    <br><br>
-    <form action="enlaces.php" method="post">
-        <label for="username">Nombre de Usuario: </label>
-        <input type="text" name="username">
-        <br>
-        <label for="password">Contraseña</label>
-        <input type="text" name="password">
-        <br>
-        <input type="submit" value="Send">
-    </form>
-
+  <h1>Pagina inicio modelos</h1>
+  <p>Entrar con ejemplo - ejemplo (user - password)</p>
+  <br><br>
+  <form action="enlaces.php" method="post">
+    <label for="username">Nombre de Usuario: </label>
+    <input type="text" name="username">
+    <br>
+    <label for="password">Contraseña</label>
+    <input type="text" name="password">
+    <br>
+    <input type="submit" value="Send">
+  </form>
 </body>
 </html>
