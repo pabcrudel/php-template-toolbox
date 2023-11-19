bc259ba8d19471afd3f22eacca8276205e0f4312
pabcrudel <pabcrudel@gmail.com>
2023-11-19 17:26:35 +0100

    add: Footer with some app links and user data

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+<?php
+// Links to navigate throw pages...
+echo "<br><a href='display-data.php'>Display data</a><br>\n";
+echo "<br><a href='index.php'>Change user</a><br>\n";
+
+require_once 'display-user-data';
+?>
