452057e25fb71881b4e844a3e26ce68b0405c1dc
pabcrudel <pabcrudel@gmail.com>
2023-11-20 19:42:49 +0100

    change: Call createDB method in a separated script

--- database/create-database.php
+++ database/create-database.php
@@ -53,36 +53,4 @@ function createDatabase($dbName, $tables) {
   
   echo "End of the program.<br>\n";
 }
-
-// Define the structure of the tables and the data to insert
-$tables = [
-  "administrators" => [
-    "structure" => [
-      "user VARCHAR(20) PRIMARY KEY NOT NULL",
-      "password VARCHAR(20) NOT NULL"
-    ],
-    "records" => [
-      ["admin", "admin"],
-    ],
-  ],
-  "articles" => [
-    "structure" => [
-      "id INT AUTO_INCREMENT PRIMARY KEY NOT NULL",
-      "name VARCHAR(20) NOT NULL",
-      "registration_date DATETIME NOT NULL",
-      "price DECIMAL(6,2) NOT NULL",
-      "description TEXT NOT NULL",
-    ],
-    "records" => [
-      [null, "Cartridge", "2007-12-31", 6.50, "Black HP Cartridge"],
-      [null, "Laser Printer", "2008-01-01", 120, "High capacity and speed"]
-    ],
-  ],
-];
-
-// Database name
-$dbName = "products";
-
-// How to call the database creation script
-createDatabase($dbName, $tables);
 ?>
