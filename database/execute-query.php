1e16c9f2d8232591cab0e21cd8afa49192da6ba9
pabcrudel <pabcrudel@gmail.com>
2023-11-20 19:43:41 +0100

    change: Execute script is not only for tables

--- database/execute-query.php
+++ database/execute-query.php
@@ -1,8 +1,8 @@
 <?php
-function executeQuery($connection, $query, $table) {
+function executeQuery($connection, $query, $target) {
   $result = mysqli_query($connection, $query);
   if (!$result) {
-    die("ERROR: Can not execute query on \"$table\"<br>\n");
+    die("ERROR: Can not execute query on \"$target\"<br>\n");
   }
 
   return $result;
